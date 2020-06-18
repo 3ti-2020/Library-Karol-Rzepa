21670dae3ee5f134c662bc389a00828748a2d9d7
PapcioRzepcia <42960600+PapcioRzepcia@users.noreply.github.com>
2020-06-19 01:56:31 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -13,7 +13,31 @@
         </div>
         <div class="main">
             <div class="a">
+            <?php
+           $conn = new mysqli("localhost", "root", "", "library", "3308");
+           $result1 = $conn->query("SELECT id_krzyz, autor, tytul FROM krzyz, autorzy, tytuly WHERE krzyz.id_autor=autorzy.id_autor AND krzyz.id_tytul=tytuly.id_tytul");
+           echo("<table class='tabelka' border=1");
+           echo("<tr>
+           <th>ID Książki</th>
+           <th>Autor</th>
+           <th>Tytuł</th>
+           <th>Usuwanie</th>
+           </tr>");
 
+           while($row=$result1->fetch_assoc() ){
+               echo("<tr>");
+               echo("<td>".$row['id_krzyz']."</td>");
+               echo("<td>".$row['autor']."</td>");
+               echo("<td>".$row['tytul']."</td>");
+               echo("<td>
+               <form action='delete.php' method='post'>
+                   <input type='hidden' name='id' value='".$row['id_krzyz']."'>
+                   <input type='submit' value='x'>
+               </form>
+                      </td>");
+               echo("</tr>");
+           }
+          ?>
             </div>
             <div class="b">
 
